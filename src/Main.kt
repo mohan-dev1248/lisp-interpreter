0a0190d44d5d3696e10147ad99436c78d34efb4c
mohan-dev1248 <mohanakrishna96@gmail.com>
2019-04-16 11:15:07 +0530

    First commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+import java.io.*
+
+val regex ="^(-?[1-9][0-9]*(([.][0-9]+)?([eE][+-]?[0-9]+)?)?)|^(-?0[.][0-9]+([eE][+-]?[0-9]+)?)|^(0)".toRegex()
+
+fun main(args: Array<String>){
+    val lines = File("./input.txt").readLines()
+
+    //evalNumber(text)
+}
+
+fun evalNumber(string: String){
+
+}
